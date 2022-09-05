91dc9355d1c915c4ce31f27cac8889796af7b7e8
Odélia Guedj <47503401+odeliaG@users.noreply.github.com>
2022-09-05 11:44:47 +0200

    Update HierNetProj.source.R

--- HierNetProj/HierNetProj.source.R
+++ HierNetProj/HierNetProj.source.R
@@ -1,3 +1,14 @@
+withdraw_proj = function(xtilde, pmain){
+  p2 = pmain + (pmain) * (pmain+1)/2
+  xtilde2 = xtilde
+  for (j in ((pmain+1):(pmain + (pmain) * (pmain+1)/2))){
+    model_j = lm(xtilde[,j]~as.matrix(xtilde[,1:pmain]))
+    xtilde2[,j] = xtilde[,j] - predict(model_j)
+  }
+  return(xtilde2)
+}
+
+
 HierNetProj = function(x, y){
   x = as.matrix(x)
   pmain = ncol(x)
